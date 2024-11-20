fb6668702c610008681e819933a9622df7c004c2
Ayomide <ayomideayomipo147@gmail.com>
2024-11-20 10:06:59 +0100

    Adds Resturant Route aand COntroller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const connectDb = require('./data/db')
 const routee = require('./routes/userRoutes')
 const route = require('./routes/authRoutes')
+const resturantRoutes = require('./routes/resturantRoutes')
 
 const app = express()
 
@@ -19,6 +20,7 @@ app.use(express.json())
 // ROutes
 app.use('/api/v1/auth', route)
 app.use('/api/v1/user', routee)
+app.use('/api/v1/resturant', resturantRoutes)
 app.get('/', (req, res) => {
   res.status(200).send(`<h1>Welcome to Food App Server</h1>`)
 })
